5dde8244ba29f4982672524ab658cb44dd60692a
mroefs1 <michael_roefs@yahoo.com>
2025-03-27 14:47:04 -0400

    draw triangle and enable movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +8,26 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
-        screen.fill((0, 0, 0))
+
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+        
+        
+
 
 if __name__ == "__main__":
     main()
